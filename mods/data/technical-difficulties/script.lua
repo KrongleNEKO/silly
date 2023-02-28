cfd198f531d048526c5d24fbb4ecabb2db92ae75
KrongleNEKO <cutememkitty@gmail.com>
2023-02-28 15:47:51 -0600

    Blue Smiley Update
    Okay it's more than just blue smiley but still. It's got:
    Blue Smiley
    Vore (no actual vore the song was good)
    Classified
    and YOMIFunk

---
+++ mods/data/technical-difficulties/script.lua
@@ -0,0 +1,13 @@
+function onCreatePost()
+	noteTweenX("swap0", 0, defaultPlayerStrumX0, 0.1, "linear")
+	noteTweenX("swap01", 4, defaultOpponentStrumX0, 0.1, "linear")
+	
+	noteTweenX("swap1", 1, defaultPlayerStrumX1, 0.1, "linear")
+	noteTweenX("swap11", 5, defaultOpponentStrumX1, 0.1, "linear")
+	
+	noteTweenX("swap2", 2, defaultPlayerStrumX2, 0.1, "linear")
+	noteTweenX("swap21", 6, defaultOpponentStrumX2, 0.1, "linear")
+	
+	noteTweenX("swap3", 3, defaultPlayerStrumX3, 0.1, "linear")
+	noteTweenX("swap31", 7, defaultOpponentStrumX3, 0.1, "linear")
+end
